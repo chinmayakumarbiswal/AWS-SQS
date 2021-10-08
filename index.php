5342888ca4a197b48bc1ad2c180e868d72f9632b
Chinmaya Kumar Biswal <chinmayakumarbiswal16045@gmail.com>
2021-10-08 11:28:04 +0530

    Update index.php

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
 
 $queueUrl = 'https://sqs.ap-south-1.amazonaws.com/<your sqs >';
 
+//download aws sdk for autoload.php
+// composer require aws/aws-sdk-php
+
 require_once 'vendor/autoload.php';
 $sdk = new \Aws\Sdk();
 $sqsClient = $sdk->createSqs(['region' => 'ap-south-1', 'version' => '2012-11-05']);
