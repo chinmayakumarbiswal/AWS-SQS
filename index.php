19814493939eb136c10dae6a3b13f848ca683499
Chinmaya Kumar Biswal <chinmayakumarbiswal16045@gmail.com>
2021-10-15 15:54:39 +0530

    Update index.php

--- index.php
+++ index.php
@@ -15,10 +15,37 @@
         'region' => 'ap-south-1',
         'version' => 'latest'
         ));
-        
+
+
+        echo "Sending message\n";
         $client->sendMessage(array(
             'QueueUrl' => 'https://sqs.ap-south-1.amazonaws.com/<sqs url>', // write your SQS url here
             'MessageBody' => 'Your Message here', // write your message here.
          ));
-        
+
+
+        echo "Get message\n";
+        $res = $client->receiveMessage(array(
+                        'QueueUrl'          => 'https://sqs.ap-south-1.amazonaws.com/<sqs url>',
+                        'WaitTimeSeconds'   => 1
+                    ));
+                    if ($res->getPath('Messages')) {
+                
+                
+                        foreach ($res->getPath('Messages') as $msg) {
+                            echo "Received Msg: ".$msg['Body']."<br>";
+                            echo "MessageId".$msg['MessageId']."<br>";
+                            // Do something useful with $msg['Body'] here
+                            echo "Delete message\n";
+                            $res = $client->deleteMessage(array(
+                                'QueueUrl'      => 'https://sqs.ap-south-1.amazonaws.com/<sqs url>',
+                                'ReceiptHandle' => $msg['ReceiptHandle']
+                            ));
+                        }
+                    }
+                    else
+                    {
+                            echo "no data found";
+                    }
+
 ?>
